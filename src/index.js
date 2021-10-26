50938d2a021a99abac690a7f02cebeeb749df9b6
Viktor Hesselbom <viktor@hesselbom.net>
2021-10-26 16:48:48 +0200

    Add encoding snapshot tests

--- src/index.js
+++ src/index.js
@@ -3,19 +3,42 @@
 
 const cuid = require('cuid')
 const encoding = require('lib0/dist/encoding.cjs')
+const decoding = require('lib0/dist/decoding.cjs')
 
-function VDoc () {
+function VDoc (options) {
   const map = new Map()
-  const localClientId = cuid()
+  const localClientId = (options && options.clientId) || cuid()
+
+  const clearFromTimestamp = (timestamp) => {
+    for (const [key, value] of map.entries()) {
+      if (value.timestamp < timestamp) {
+        map.delete(key)
+      }
+    }
+  }
 
   return {
     clientId: localClientId,
-    set: function (key, data, timestamp, updateClientId) {
-      const clientId = updateClientId || localClientId
+    set: function (key, data, timestamp, clientId) {
       const existing = map.get(key)
+      const clearedFromTimestamp = map.get('_clearedFromTimestamp')
+
+      clientId = clientId == null ? localClientId : clientId
+      timestamp = timestamp == null ? Date.now() : timestamp
+
+      // Check if timestamp is before cleared timestamp
+      if (clearedFromTimestamp && timestamp < clearedFromTimestamp.data) {
+        return
+      }
 
       if (!existing) {
         map.set(key, { timestamp, data, clientId })
+
+        // Clear all data prior to this timestamp
+        if (key === '_clearedFromTimestamp') {
+          clearFromTimestamp(data)
+        }
+
         return
       }
 
@@ -28,12 +51,22 @@ function VDoc () {
       if (timestamp === existing.timestamp && clientId !== existing.clientId) {
         if (clientId > existing.clientId) {
           map.set(key, { timestamp, data, clientId })
+
+          // Clear all data prior to this timestamp
+          if (key === '_clearedFromTimestamp') {
+            clearFromTimestamp(data)
+          }
         }
         return
       }
 
       if (timestamp >= existing.timestamp) {
         map.set(key, { timestamp, data, clientId })
+
+        // Clear all data prior to this timestamp
+        if (key === '_clearedFromTimestamp') {
+          clearFromTimestamp(data)
+        }
       }
     },
     remove: function (key, timestamp, clientId) {
@@ -55,7 +88,7 @@ function VDoc () {
 
       map.forEach((value, key) => {
         if (value.timestamp >= timestamp) {
-          obj[key] = value.data
+          obj[key] = value
         }
       })
 
@@ -69,17 +102,54 @@ function VDoc () {
           if (value.data === null) {
             encoding.writeUint8(encoder, 0)
             encoding.writeVarString(encoder, key)
+            encoding.writeFloat64(encoder, value.timestamp)
+            encoding.writeVarString(encoder, value.clientId)
           } else {
             encoding.writeUint8(encoder, 1)
             encoding.writeVarString(encoder, key)
-            encoding.writeVarString(encoder, value.data)
+            encoding.writeFloat64(encoder, value.timestamp)
+            encoding.writeVarString(encoder, value.clientId)
+            encoding.writeAny(encoder, value.data)
           }
         }
       })
 
       return encoding.toUint8Array(encoder)
+    },
+    clearFromTimestamp: function (fromTimestamp, timestamp, updateClientId) {
+      this.set('_clearedFromTimestamp', fromTimestamp, timestamp, updateClientId)
+    },
+    applySnapshot: function (snapshot) {
+      for (const [key, value] of Object.entries(snapshot)) {
+        this.set(key, value.data, value.timestamp, value.clientId)
+      }
     }
   }
+}
+
+VDoc.decodeSnapshot = function decodeSnapshot (byteArray) {
+  const decoder = decoding.createDecoder(byteArray)
+  const snapshot = {}
+
+  while (decoder.pos < decoder.arr.length) {
+    const hasData = decoding.readUint8(decoder) === 1
+    const key = decoding.readVarString(decoder)
+
+    const object = {
+      timestamp: decoding.readFloat64(decoder),
+      clientId: decoding.readVarString(decoder)
+    }
+
+    if (hasData) {
+      object.data = decoding.readAny(decoder)
+    } else {
+      object.data = null
+    }
+
+    snapshot[key] = object
+  }
+
+  return snapshot
 }
 
 module.exports = VDoc
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,5 @@
 /* eslint-env jest */
 const VDoc = require('./index')
-const encoding = require('lib0/dist/encoding.cjs')
 
 test('set keys', () => {
   const doc = VDoc()
@@ -93,21 +92,31 @@ test('remove if removed timestamp is later even if received before', () => {
   expect(doc.toJSON()).toEqual({})
 })
 
+test('if timestamp is missing, use Date.now()', () => {
+  const doc = VDoc({ clientId: 'clientId' })
+  const then = Date.now()
+
+  doc.set('key', 'data')
+
+  // Test if timestamp is later than the first Date.now() we got
+  expect(doc.getSnapshotFromTimestamp(0).key.timestamp).toBeGreaterThanOrEqual(then)
+})
+
 test('get diff snapshot after specific timestamp', () => {
-  const doc = VDoc()
+  const doc = VDoc({ clientId: 'clientId' })
 
   doc.set('key', 'data', 1000)
   doc.set('key2', 'data', 1500)
   doc.remove('key', 2000)
 
   expect(doc.getSnapshotFromTimestamp(1500)).toEqual({
-    key2: 'data',
-    key: null
+    key2: { timestamp: 1500, data: 'data', clientId: 'clientId' },
+    key: { timestamp: 2000, data: null, clientId: 'clientId' }
   })
 })
 
 test('get diff snapshot after specific timestamp, making sure deletes are not included if old', () => {
-  const doc = VDoc()
+  const doc = VDoc({ clientId: 'clientId' })
 
   doc.set('key', 'data', 1000)
   doc.set('key2', 'data', 1500)
@@ -116,24 +125,125 @@ test('get diff snapshot after specific timestamp, making sure deletes are not in
   expect(doc.toJSON()).toEqual({ key2: 'data' })
 
   expect(doc.getSnapshotFromTimestamp(1500)).toEqual({
-    key2: 'data'
+    key2: { timestamp: 1500, data: 'data', clientId: 'clientId' }
   })
 })
 
-test('get diff snapshot encoded as uint8 after specific timestamp', () => {
-  const doc = VDoc()
+test('get diff snapshot encoded as uint8 after specific timestamp, and decode', () => {
+  const doc = VDoc({ clientId: 'clientId' })
 
   doc.set('key', 'data', 1000)
-  doc.set('key2', 'data', 1500)
+  doc.set('key2', 'data', 1635257645564)
   doc.remove('key', 2000)
 
-  const encoder = encoding.createEncoder()
-  encoding.writeUint8(encoder, 0)
-  encoding.writeVarString(encoder, 'key')
-  encoding.writeUint8(encoder, 1)
-  encoding.writeVarString(encoder, 'key2')
-  encoding.writeVarString(encoder, 'data')
+  const resultSnapshot = doc.getSnapshotFromTimestamp(1500)
+  const byteArray = doc.getEncodedSnapshotFromTimestamp(1500)
+  const decodedSnapshot = VDoc.decodeSnapshot(byteArray)
 
-  expect(doc.getEncodedSnapshotFromTimestamp(1500))
-    .toEqual(encoding.toUint8Array(encoder))
+  // Make sure we get byte array
+  expect(byteArray)
+    .toBeInstanceOf(Uint8Array)
+
+  // Verify that both snapshot and decoded snapshot are the same
+  expect(resultSnapshot).toEqual({
+    key2: { timestamp: 1635257645564, data: 'data', clientId: 'clientId' },
+    key: { timestamp: 2000, data: null, clientId: 'clientId' }
+  })
+
+  expect(decodedSnapshot).toEqual({
+    key2: { timestamp: 1635257645564, data: 'data', clientId: 'clientId' },
+    key: { timestamp: 2000, data: null, clientId: 'clientId' }
+  })
+})
+
+test('handle encode/decode of various types', () => {
+  const doc = VDoc({ clientId: 'clientId' })
+
+  doc.set('string', 'data', 1000)
+  doc.set('number', 10, 1000)
+  doc.set('boolean', true, 1000)
+  doc.set('object', { foo: 'bar' }, 1000)
+
+  const resultSnapshot = doc.getSnapshotFromTimestamp(0)
+  const byteArray = doc.getEncodedSnapshotFromTimestamp(0)
+  const decodedSnapshot = VDoc.decodeSnapshot(byteArray)
+
+  // Make sure we get byte array
+  expect(byteArray)
+    .toBeInstanceOf(Uint8Array)
+
+  // Verify that both snapshot and decoded snapshot are the same
+  expect(resultSnapshot).toEqual({
+    string: { timestamp: 1000, data: 'data', clientId: 'clientId' },
+    number: { timestamp: 1000, data: 10, clientId: 'clientId' },
+    boolean: { timestamp: 1000, data: true, clientId: 'clientId' },
+    object: { timestamp: 1000, data: { foo: 'bar' }, clientId: 'clientId' }
+  })
+
+  expect(decodedSnapshot).toEqual({
+    string: { timestamp: 1000, data: 'data', clientId: 'clientId' },
+    number: { timestamp: 1000, data: 10, clientId: 'clientId' },
+    boolean: { timestamp: 1000, data: true, clientId: 'clientId' },
+    object: { timestamp: 1000, data: { foo: 'bar' }, clientId: 'clientId' }
+  })
+})
+
+test('clear all data from timestamp, to clean up', () => {
+  const doc = VDoc({ clientId: 'clientId' })
+
+  doc.set('key1', 'data', 1000) // will be cleared
+  doc.set('key2', 'data', 1500) // will stay
+
+  doc.clearFromTimestamp(1499, 1500) // everything before this is removed
+
+  doc.set('key3', 'data', 1000) // will not be added at all
+  doc.set('key4', 'data', 1499) // will be added because it's precisely at timestamp clear
+
+  expect(doc.toJSON()).toEqual({
+    _clearedFromTimestamp: 1499,
+    key2: 'data',
+    key4: 'data'
+  })
+
+  expect(doc.getSnapshotFromTimestamp(0)).toEqual({
+    _clearedFromTimestamp: { timestamp: 1500, data: 1499, clientId: 'clientId' },
+    key2: { timestamp: 1500, data: 'data', clientId: 'clientId' },
+    key4: { timestamp: 1499, data: 'data', clientId: 'clientId' }
+  })
+})
+
+test('merge snapshot to document', () => {
+  const docA = VDoc()
+  docA.set('key1', 'dataA', 1000)
+  docA.set('key2', 'dataA', 1500)
+
+  const docB = VDoc()
+  docB.set('key1', 'dataB', 1001)
+  docB.set('key2', 'dataB', 1499)
+
+  docA.applySnapshot(docB.getSnapshotFromTimestamp(0))
+
+  expect(docA.toJSON()).toEqual({
+    key1: 'dataB',
+    key2: 'dataA'
+  })
+})
+
+test('merge snapshot to document with _clearedFromTimestamp', () => {
+  const docA = VDoc()
+  docA.set('key1', 'dataA', 1000)
+  docA.set('key2', 'dataA', 1500)
+  docA.clearFromTimestamp(1498)
+
+  const docB = VDoc()
+  docB.set('key1', 'dataB', 1001)
+  docB.set('key2', 'dataB', 1499)
+
+  docA.applySnapshot(docB.getSnapshotFromTimestamp(0))
+
+  expect(docA.toJSON()).toEqual({
+    _clearedFromTimestamp: 1498,
+    // No key1 since it's cleared
+    key2: 'dataA'
+  })
 })
