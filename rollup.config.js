c94725d55c2db3ce18f53555c6533b4db8383db2
Viktor Hesselbom <viktor@hesselbom.net>
2021-11-18 10:06:14 +0100

    Add extra export to work with react-native's bundler

--- rollup.config.js
+++ rollup.config.js
@@ -15,5 +15,21 @@ export default [
       resolve({ browser: true }),
       commonjs()
     ]
+  },
+
+  // Add .js to work with react-native
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'CrdtMap',
+      file: 'dist/crdtmap.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'default'
+    },
+    plugins: [
+      resolve({ browser: true }),
+      commonjs()
+    ]
   }
 ]
